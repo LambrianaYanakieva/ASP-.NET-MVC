50243f1745460b26f6737c9befaea118ca255199
LambrianaYanakieva <lambrianayanakieva@gmail.com>
2017-10-13 15:27:06 +0300

    dbRepository added

---
+++ TaskManager/src/TaskManager/TaskManager.Data.Common/Repositories/Contracts/IDbRepository{T}.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace TaskManager.Data.Common.Repositories.Contracts
+{
+    public interface IDbRepository<T>
+        where T : class
+    {
+        IQueryable<T> All();
+
+        IQueryable<T> AllWithDeleted();
+
+        T GetById(object id);
+
+        void Add(T entity);
+
+        void Delete(T entity);
+
+        void HardDelete(T entity);
+
+        void Dispose();
+    }
+}
---
+++ TaskManager/src/TaskManager/TaskManager.Data.Common/Repositories/DbRepository{T}.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Data.Common.Base;
+using System.Data.Entity;
+using TaskManager.Data.Common.Repositories.Contracts;
+
+namespace TaskManager.Data.Common.Repositories
+{
+    public class DbRepository<T> : IDbRepository<T>
+       where T : BaseModel<int>
+    {
+        public DbRepository(DbContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentException("An instance of DbContext is required to use this repository.", nameof(context));
+            }
+
+            this.Context = context;
+            this.DbSet = this.Context.Set<T>();
+        }
+
+        private IDbSet<T> DbSet { get; }
+
+        private DbContext Context { get; }
+
+        public IQueryable<T> All()
+        {
+            return this.DbSet.Where(x => !x.IsDeleted);
+        }
+
+        public IQueryable<T> AllWithDeleted()
+        {
+            return this.DbSet;
+        }
+
+        public T GetById(object id)
+        {
+            var item = this.DbSet.Find(id);
+            if (item.IsDeleted)
+            {
+                return null;
+            }
+
+            return item;
+        }
+
+        public void Add(T entity)
+        {
+            this.DbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            entity.IsDeleted = true;
+            entity.DeletedOn = DateTime.UtcNow;
+        }
+
+        public void HardDelete(T entity)
+        {
+            this.DbSet.Remove(entity);
+        }
+
+        public void Dispose()
+        {
+            this.Context.Dispose();
+        }
+    }
+}
